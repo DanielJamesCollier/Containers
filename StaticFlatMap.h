b19eb45bbb04995ae87158ac0f6921f639036b19
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-01-09 13:39:18 +0000

    re-write
    removed dynamic alloc - unified error handling

--- StaticFlatMap.h
+++ StaticFlatMap.h
@@ -1,185 +1,156 @@
-#pragma once
-#include <algorithm>
-#include <iostream>	
+#ifndef StaticFlatMap_h
+#define StaticFlatMap_h
+#define NOT_MAPPED -1
 
-/*
-	the m_forward reverse map stores to mappings in one contiguous memory block
+#include <iostream>
+#include <memory>
+#include <array>
+#include <type_traits>
 
-	m_forwardReverseMap[0 to (m_poolSize -1)]        = the forward map
-	m_forwardReverseMap[m_poolSize to m_poolSize -1] = the reverse map
-*/
+enum class MapError {
+    OKAY,
+    FULL,
+    ID_GREATER_THAN_POOLSIZE,
+    ID_GREATER_THAN_PARTITION,
+    OBJECT_DOES_NOT_EXIST,
+};
 
-template <class T>
+template <typename T, size_t poolSize>
 class StaticFlatMap
 {
 public:
-	explicit StaticFlatMap(size_t poolSize)
-		:
-		m_poolSize(poolSize),
-		m_partition(0),
-		m_pool(new T[poolSize]),
-		m_forwardReverseMap(new int[poolSize * 2]),
-		m_failReturn(T())
-	{
-		// initialiastation
-		for (size_t i = 0; i < poolSize; i++)
-		{
-			m_forwardReverseMap[i] = m_forwardReverseMap[i + poolSize] = -1;
-		}
-	}
-
-	~StaticFlatMap()
-	{
-		delete[] m_pool, m_forwardReverseMap;
-	}
-
-	int AddObject(T object)
-	{
-		if (m_partition == m_poolSize) return -1; // check if the pool is full befor adding an object 
-
-		m_forwardReverseMap[m_partition] = m_forwardReverseMap[m_partition + m_poolSize] = m_partition;
-		m_pool[m_partition] = object;
-		return m_partition++;
-	}
-
-	int AddObject(unsigned int id, T object)
-	{
-		if (m_partition == m_poolSize) return -1; // check if the pool is full befor adding an object 
-
-		if (m_forwardReverseMap[id] != -1) return -1;
-
-		m_forwardReverseMap[id] = m_partition;
-		m_forwardReverseMap[m_partition + m_poolSize] = id;
-
-		m_pool[m_partition] = object;
-		return m_partition++;
-	}
-
-
-	T& GetObjectWithStaticID(unsigned int id, bool& valid)
-	{
-		int componentPoolIndex = m_forwardReverseMap[id];
-
-		if (id > m_poolSize || componentPoolIndex == -1 || componentPoolIndex > m_partition)
-		{
-			valid = false;
-			return m_failReturn;
-		}
-
-		valid = true;
-		return m_pool[componentPoolIndex];
-	}
-
-	T& GetObjectWithPoolID(unsigned int id, bool& valid)
-	{
-		if (id > m_partition || id > m_poolSize)
-		{
-			valid = false;
-			return m_failReturn;
-		}
-
-		valid = true;
-		return m_pool[id];
-	}
-
-	T& GetObjectWithPoolID(unsigned int id,unsigned int& staticID, bool& valid)
-	{
-		if (id > m_partition || id > m_poolSize)
-		{
-			valid    = false;
-			staticID = 0;
-			return m_failReturn;
-		}
-
-		valid    = true;
-		staticID = m_forwardReverseMap[id + m_poolSize];
-
-		return m_pool[id];
-	}
-	
-	void RemoveObject(unsigned int index)
-	{
-		if (index > m_poolSize)
-		{
-			std::cout << "greater than pool size" << std::endl;
-			return;
-		}
-
-		int poolIndextoDelete = m_forwardReverseMap[index];
-
-		// there is no object mapped so return
-		if (poolIndextoDelete == -1) 
-		{
-			std::cout << "the object at " << index << " could not be deleted as it does not exits" << std::endl;
-			return;
-		}
-
-		std::swap(m_pool[poolIndextoDelete], m_pool[--m_partition]);
-
-		// re-map after swap
-		unsigned int test = m_forwardReverseMap[m_partition + m_poolSize];
-		m_forwardReverseMap[test] = poolIndextoDelete;
-		m_forwardReverseMap[index] = m_forwardReverseMap[m_partition + m_poolSize] = -1;
-
-		if (poolIndextoDelete != m_partition)
-		{
-			m_forwardReverseMap[poolIndextoDelete + m_poolSize] = test;
-		}
-		else
-		{
-			m_forwardReverseMap[poolIndextoDelete + m_poolSize] = -1;
-		}
-	}
-
-	unsigned int GetMaxPoolSizeInElements() const
-	{
-		return m_poolSize;
-	}
-
-	// get the number of items store in the pool
-	unsigned int GetPoolSizeInElements() const
-	{
-		return m_partition;
-	}
-
-	// returns the size of the pool
-	unsigned int GetPoolSizeInBytes() const
-	{
-		return (sizeof(T)) * m_poolSize;
-	}
-
-	// gets the size of the pool plus the size of the data required from handle auto defragmentation 
-	unsigned int GetPoolSizeImplementation() const
-	{
-
-	}
-
-	void PrintPool() const
-	{
-		std::cout << "  Rev   |  Forw  |  Pool " << std::endl;
-		for (unsigned int i = 0; i < 20; i++)
-		{
-			
-			std::cout << "| " << i << " - " << m_forwardReverseMap[i + m_poolSize] << " | "; // prints first row reverse map
-			std::cout << i << " -  " << m_forwardReverseMap[i] << " | "; // prints the forward mapp
-			if (i < m_partition)
-			{
-				std::cout << i << "- A";
-			}
-			else
-			{
-				std::cout << i << "- D";
-			}
-			std::cout << std::endl;
-		}
-	}
-
+    StaticFlatMap()
+    :
+		  m_poolSize{poolSize}
+    ,     m_partition{0}
+    ,     m_failReturn{}
+    {
+        m_forwardReverseMap.fill(NOT_MAPPED);
+    }
+    
+    StaticFlatMap(StaticFlatMap &)  = default;
+    StaticFlatMap(StaticFlatMap &&) = default;
+    ~StaticFlatMap()                = default;
+    
+    MapError addObject(T const & object) {
+        if (m_partition == m_poolSize) {
+            return MapError::FULL; // check if the pool is full before adding an object
+        }
+        
+        m_forwardReverseMap[m_partition]              = m_partition;
+        m_forwardReverseMap[m_partition + m_poolSize] = m_partition;
+        m_pool[m_partition]                           = object;
+        
+        m_partition++;
+        
+        return MapError::OKAY;
+    }
+    
+    MapError addObject(T const & object, size_t & staticID) {
+        if (m_partition == m_poolSize) {
+            return MapError::FULL; // check if the pool is full before adding an object
+        }
+        
+        m_forwardReverseMap[m_partition]              = m_partition;
+        m_forwardReverseMap[m_partition + m_poolSize] = m_partition;
+        m_pool[m_partition]                           = object;
+        staticID                                      = m_partition;
+        
+        m_partition++;
+        
+        return MapError::OKAY;
+    }
+    
+    MapError getObjectWithStaticID(size_t staticID, T & object) {
+        int forwardMapping = m_forwardReverseMap[staticID];
+        
+        if (staticID > m_poolSize) {
+            object = m_failReturn;
+            return MapError::ID_GREATER_THAN_POOLSIZE;
+        } else if(staticID > m_partition) {
+            object = m_failReturn;
+            return MapError::ID_GREATER_THAN_PARTITION;
+        } else if(forwardMapping == NOT_MAPPED) {
+            object = m_failReturn;
+            return MapError::OBJECT_DOES_NOT_EXIST;
+        }
+        
+        object = m_pool[forwardMapping];
+        return MapError::OKAY;
+    }
+    
+    MapError getObjectWithPoolID(size_t id, T & object) {
+        if (id > m_partition) {
+            object = m_failReturn;
+            return MapError::ID_GREATER_THAN_PARTITION;
+        } else if (id > m_poolSize) {
+            object = m_failReturn;
+            return MapError::ID_GREATER_THAN_POOLSIZE;
+        }
+        
+        object = m_pool[id];
+        return MapError::OKAY;
+    }
+    
+    MapError removeObjectStaticID(size_t staticID) {
+        int poolIndextoDelete = m_forwardReverseMap[staticID];
+        
+        if(staticID > m_partition) {
+            return MapError::ID_GREATER_THAN_PARTITION;
+        } else if (staticID > m_poolSize) {
+            return MapError::ID_GREATER_THAN_POOLSIZE;
+        } else if (poolIndextoDelete == NOT_MAPPED) {
+            return MapError::OBJECT_DOES_NOT_EXIST;
+        }
+        
+        std::swap(m_pool[poolIndextoDelete], m_pool[--m_partition]);
+        
+        // re-map after swap
+        int reverseMapMapping                  = m_forwardReverseMap[m_partition + m_poolSize];
+        m_forwardReverseMap[reverseMapMapping] = poolIndextoDelete;
+        m_forwardReverseMap[staticID]          = m_forwardReverseMap[m_partition + m_poolSize] = NOT_MAPPED;
+        
+        if (poolIndextoDelete != m_partition) {
+            m_forwardReverseMap[poolIndextoDelete + m_poolSize] = reverseMapMapping;
+        } else {
+            m_forwardReverseMap[poolIndextoDelete + m_poolSize] = NOT_MAPPED;
+        }
+        
+        return MapError::OKAY;
+    }
+    
+    size_t getPoolSizeInElements() const {
+        return m_poolSize;
+    }
+    
+    size_t getPoolSizeInActiveElements() const {
+        return m_partition;
+    }
+    
+    void printPool(size_t printDepth = poolSize) const {
+        if(printDepth > poolSize) {
+            printDepth = poolSize;
+        }
+        
+        for (size_t i = 0; i < printDepth; i++) {
+            
+            std::cout << "| " << i << " - " << m_forwardReverseMap[i + m_poolSize]  <<" | "; // print("|index - reverseMap[index]|")
+            std::cout << i << " - " << m_forwardReverseMap[i] << " | "; // prints the forward mapp
+            
+            if (i < m_partition) {
+                std::cout << i << "- A";
+            } else {
+                std::cout << i << "- D";
+            }
+            std::cout << std::endl;
+        }
+    }
+    
 private:
-	int* m_forwardReverseMap;
-	T*	 m_pool;
-	T    m_failReturn;
-
-	size_t m_poolSize;
-	size_t m_partition; // dead alive partition
+    std::array<int, poolSize * 2> m_forwardReverseMap;
+    std::array<T, poolSize>       m_pool;
+    T                             m_failReturn;
+    int                           m_poolSize;
+    int                           m_partition;
 };
-
+#endif // StaticFlatMap_h
